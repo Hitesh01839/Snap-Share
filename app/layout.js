1d869b7cdb60b846681d74a3cb729593be50e17f
Hitesh01839 <gangahitesh04@gmail.com>
2024-03-08 22:27:54 +0530

    updated layout

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Photo Store", 
+  title: "Snap Share",
 };
 
 export default function RootLayout({ children }) {
